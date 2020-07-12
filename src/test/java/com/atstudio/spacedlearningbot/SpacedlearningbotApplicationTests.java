2651d5d2494501e9ad41bdb6ef5e0d37e8c7298d
Anton Tkachenko <tkachenkoas@gmail.com>
2020-07-12 23:36:02 +0300

    initial commit + local run with embedded mongo

---
+++ src/test/java/com/atstudio/spacedlearningbot/SpacedlearningbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atstudio.spacedlearningbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpacedlearningbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
