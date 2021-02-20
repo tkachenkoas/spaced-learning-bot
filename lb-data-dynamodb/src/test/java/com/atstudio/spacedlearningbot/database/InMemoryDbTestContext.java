7bf300dc95fac69dae6552e0f004df6efecfbb7f
Anton Tkachenko <anton.tkachenko@duda.co>
2021-02-20 21:46:58 +0200

    start-stop inmemory server

--- lb-data-dynamodb/src/test/java/com/atstudio/spacedlearningbot/database/InMemoryDbTestContext.java
+++ lb-data-dynamodb/src/test/java/com/atstudio/spacedlearningbot/database/InMemoryDbTestContext.java
@@ -1,9 +1,20 @@
 package com.atstudio.spacedlearningbot.database;
 
+import com.atstudio.spacedlearningbot.database.entity.DynamoDbStarter;
+import lombok.SneakyThrows;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+
+import javax.annotation.PreDestroy;
 
 @ComponentScan("com.atstudio.spacedlearningbot.database")
 @SpringBootConfiguration
 public class InMemoryDbTestContext {
+
+    @PreDestroy
+    @SneakyThrows
+    public void stopServer() {
+        DynamoDbStarter.dynamoDbServer().stop();
+    }
+
 }
--- lb-data-dynamodb/src/test/java/com/atstudio/spacedlearningbot/database/entity/DynamoDbStarter.java
+++ lb-data-dynamodb/src/test/java/com/atstudio/spacedlearningbot/database/entity/DynamoDbStarter.java
@@ -3,30 +3,23 @@ package com.atstudio.spacedlearningbot.database.entity;
 import com.amazonaws.services.dynamodbv2.local.main.ServerRunner;
 import com.amazonaws.services.dynamodbv2.local.server.DynamoDBProxyServer;
 import lombok.SneakyThrows;
+import org.apache.commons.cli.ParseException;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PreDestroy;
-
 @Component
 public class DynamoDbStarter implements BeanFactoryPostProcessor {
-
-    private static DynamoDBProxyServer sServer;
 
     @Override
     @SneakyThrows
     public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
-        final String[] localArgs = {"-inMemory", "-port", "8084"};
-        sServer = ServerRunner.createServerFromCommandLineArgs(localArgs);
-        sServer.start();
+        dynamoDbServer().start();
     }
 
-    @PreDestroy
-    @SneakyThrows
-    public void shutdownDynamoDB() {
-        sServer.stop();
+    public static DynamoDBProxyServer dynamoDbServer() throws ParseException {
+        return ServerRunner.createServerFromCommandLineArgs(new String[]{"-inMemory", "-port", "8084"});
     }
 
 }
